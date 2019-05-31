c344b43000581b8ff1f9cdc411eaa08ea97a6e07
Arnout Engelen <arnout@bzzt.net>
2019-05-31 17:15:51 +0200

    wrrrong

--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -28,8 +28,6 @@ object AkkaBuild {
     scalacOptions in Compile ++= (
         // -release 8 is not enough, for some reason we need the 8 rt.jar explicitly #25330
         Seq("-release", "8")),
-    scalacOptions in Test := (scalacOptions in Test).value.filterNot(opt =>
-      opt == "-Xlog-reflective-calls" || opt.contains("genjavadoc")),
 
     crossVersion := CrossVersion.binary,
 
@@ -42,113 +40,6 @@ object AkkaBuild {
         case _                       => sourceDir / "scala-2.13-"
       }
     },
-
-    ivyLoggingLevel in ThisBuild := UpdateLogging.Quiet,
-
-    licenses := Seq(("Apache License, Version 2.0", url("http://www.apache.org/licenses/LICENSE-2.0"))),
-    homepage := Some(url("https://akka.io/")),
-
-    apiURL := Some(url(s"https://doc.akka.io/api/akka/${version.value}")),
-
-    initialCommands :=
-      """|import language.postfixOps
-         |import akka.actor._
-         |import scala.concurrent._
-         |import com.typesafe.config.ConfigFactory
-         |import scala.concurrent.duration._
-         |import akka.util.Timeout
-         |var config = ConfigFactory.parseString("akka.stdout-loglevel=INFO,akka.loglevel=DEBUG,pinned{type=PinnedDispatcher,executor=thread-pool-executor,throughput=1000}")
-         |var remoteConfig = ConfigFactory.parseString("akka.remote.classic.netty{port=0,use-dispatcher-for-io=akka.actor.default-dispatcher,execution-pool-size=0},akka.actor.provider=remote").withFallback(config)
-         |var system: ActorSystem = null
-         |implicit def _system = system
-         |def startSystem(remoting: Boolean = false) { system = ActorSystem("repl", if(remoting) remoteConfig else config); println("don’t forget to system.terminate()!") }
-         |implicit def ec = system.dispatcher
-         |implicit val timeout = Timeout(5 seconds)
-         |""".stripMargin,
-
-    /**
-     * Test settings
-     */
-    fork in Test := true,
-
-    // default JVM config for tests
-    javaOptions in Test ++= {
-      val defaults = Seq(
-        // ## core memory settings
-        "-XX:+UseG1GC",
-        // most tests actually don't really use _that_ much memory (>1g usually)
-        // twice used (and then some) keeps G1GC happy - very few or to no full gcs
-        "-Xms3g", "-Xmx3g",
-        // increase stack size (todo why?)
-        "-Xss2m",
-
-        // ## extra memory/gc tuning
-        // this breaks jstat, but could avoid costly syncs to disc see http://www.evanjones.ca/jvm-mmap-pause.html
-        "-XX:+PerfDisableSharedMem",
-        // tell G1GC that we would be really happy if all GC pauses could be kept below this as higher would
-        // likely start causing test failures in timing tests
-        "-XX:MaxGCPauseMillis=300",
-        // nio direct memory limit for artery/aeron (probably)
-        "-XX:MaxDirectMemorySize=256m",
-
-        // faster random source
-        "-Djava.security.egd=file:/dev/./urandom")
-
-      if (sys.props.contains("akka.ci-server"))
-        defaults ++ Seq("-XX:+PrintGCTimeStamps", "-XX:+PrintGCDetails")
-      else
-        defaults
-    },
-
-    // all system properties passed to sbt prefixed with "akka." will be passed on to the forked jvms as is
-    javaOptions in Test := {
-      val base = (javaOptions in Test).value
-      val akkaSysProps: Seq[String] =
-        sys.props.filter(_._1.startsWith("akka"))
-          .map { case (key, value) => s"-D$key=$value" }(breakOut)
-
-      base ++ akkaSysProps
-    },
-
-    // with forked tests the working directory is set to each module's home directory
-    // rather than the Akka root, some tests depend on Akka root being working dir, so reset
-    testGrouping in Test := {
-      val original: Seq[Tests.Group] = (testGrouping in Test).value
-
-      original.map { group =>
-        group.runPolicy match {
-          case Tests.SubProcess(forkOptions) =>
-            group.copy(runPolicy = Tests.SubProcess(forkOptions.withWorkingDirectory(
-              workingDirectory = Some(new File(System.getProperty("user.dir"))))))
-          case _ => group
-        }
-      }
-    },
-
-    logBuffered in Test := System.getProperty("akka.logBufferedTests", "false").toBoolean,
-
-    // show full stack traces and test case durations
-    testOptions in Test += Tests.Argument("-oDF"),
-
-    docLintingSettings,
   )
 
-  lazy val docLintingSettings = Seq(
-  )
-
-
-  def loadSystemProperties(fileName: String): Unit = {
-    import scala.collection.JavaConverters._
-    val file = new File(fileName)
-    if (file.exists()) {
-      println("Loading system properties from file `" + fileName + "`")
-      val in = new InputStreamReader(new FileInputStream(file), "UTF-8")
-      val props = new Properties
-      props.load(in)
-      in.close()
-      sys.props ++ props.asScala
-    }
-  }
-
-  def majorMinor(version: String): Option[String] = """\d+\.\d+""".r.findFirstIn(version)
 }
