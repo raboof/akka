28de9364524fc9bcaed6170a6c30353b66bf161c
Arnout Engelen <arnout@bzzt.net>
2019-06-03 11:46:32 +0200

    simplify

--- build.sbt
+++ build.sbt
@@ -1,7 +1,14 @@
 import akka.{ AkkaBuild, Dependencies }
 
+scalacOptions in Compile ++= (
+    // -release 8 is not enough, for some reason we need the 8 rt.jar explicitly #25330
+    Seq("-release", "8"))
+
 lazy val root = Project(id = "akka", base = file("akka-actor"))
   .settings(Dependencies.actor)
   .settings(akka.AkkaBuild.buildSettings)
-  .settings(akka.AkkaBuild.defaultSettings)
-
+  .settings(
+scalacOptions in Compile ++= (
+    // -release 8 is not enough, for some reason we need the 8 rt.jar explicitly #25330
+    Seq("-release", "8"))
+)
--- project/AkkaBuild.scala
+++ project/AkkaBuild.scala
@@ -20,14 +20,4 @@ object AkkaBuild {
     Dependencies.Versions,
   )
 
-  lazy val defaultSettings: Seq[Setting[_]] = Def.settings(
-    // Makes sure that, even when compiling with a jdk version greater than 8, the resulting jar will not refer to
-    // methods not found in jdk8. To test whether this has the desired effect, compile akka-remote and check the
-    // invocation of 'ByteBuffer.clear()' in EnvelopeBuffer.class with 'javap -c': it should refer to
-    // "java/nio/ByteBuffer.clear:()Ljava/nio/Buffer" and not "java/nio/ByteBuffer.clear:()Ljava/nio/ByteBuffer":
-    scalacOptions in Compile ++= (
-        // -release 8 is not enough, for some reason we need the 8 rt.jar explicitly #25330
-        Seq("-release", "8")),
-  )
-
 }
